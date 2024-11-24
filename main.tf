1ec334bfe5e0f9fee425e31f26b81121faf97c34
David Eliason <davethemaker@gmail.com>
2024-11-24 14:39:35 -0800

    add attributes to the ec2 instance - launch configuration, security group, variable for server port, public ip output, vpc output

--- main.tf
+++ main.tf
@@ -2,11 +2,58 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-04dd23e62ed049936"
-  instance_type = "t2.micro"
+resource "aws_launch_configuration" "example" {
+  image_id           = "ami-04dd23e62ed049936"
+  instance_type      = "t2.micro"
+  security_groups    = [aws_security_group.instance.id]
 
-  tags = {
-    Name = "terraform-example"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  # Required when using a launch configuration with an auto scaling group.
+  lifecycle {
+    create_before_destroy = true
   }
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
